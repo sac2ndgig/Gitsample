ca1b9763dbbbd63981636d736c87a7a7cc23a862
Razgriz <ps3bf3@gmail.com>
2021-06-08 16:03:21 +0800

    ADD MATH BRANCH

--- GitBranchSample/src/GitBranchApp/mainApp.java
+++ GitBranchSample/src/GitBranchApp/mainApp.java
@@ -3,5 +3,13 @@ package GitBranchApp;
 public class mainApp {
 	public static void main(String[] args) {
 		System.out.println("1");
+		Math cMath =new Math();
+		System.out.println();
+		System.out.println(cMath.add(10, 20));
+	}
+}
+class Math{
+	int add(int a,int b) {
+		return a+b;
 	}
 }
